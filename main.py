084b152b512aaeec4f78915fa482215d29f1def1
misterbastean <misterbastean@gmail.com>
2019-03-12 13:44:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from pymongo import MongoClient
+from datetime import datetime
+
+baseUrl = "https://store.steampowered.com/search/?specials=1&page="
+
+# Create connection, read raw html, close connection
+uClient = uReq(baseUrl + "1")
+page_html = uClient.read()
+uClient.close()
+
+# Parse the html
+page_soup = soup(page_html, "html.parser")
+
+# Grabs list of products
+search_results = page_soup.findAll("a", {"class": "search_result_row"})
+
+for i, product in enumerate(search_results):
+    try:
+        name = product.find("span", "title").text.strip()
+        original_price = int(str(product.find("strike")).replace("<strike>", "").replace("</strike>", "").replace("$", "").replace(".", ""))
+        sale_price = int(str(product.find("div", "search_price").contents[3]).replace("$", "").replace(".", "").strip())
+
+        # Parse platforms
+        platforms = []
+        platforms_raw = product.findAll("span", "platform_img")
+        for span in platforms_raw:
+            if "win" in str(span):
+                platforms.append("win")
+            if "mac" in str(span):
+                platforms.append("mac")
+            if "linux" in str(span):
+                platforms.append("linux")
+
+        game_data = {
+                "gameName": name,
+                "platform": platforms,
+                "originalPrice": original_price,
+                "discountedPrice": [{
+                    "date": datetime.now(),
+                    "price": sale_price
+                }],
+            }
+
+        print(game_data)
+    except Exception as e:
+        print("**********Problem with iteration " + str(i) + "**********")
+        print(e)
+
+

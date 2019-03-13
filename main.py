a39bee53b2e49c209ef2c236d87bf8f176aa8349
misterbastean <misterbastean@gmail.com>
2019-03-13 08:26:20 -0400

    Add DB update or add

--- main.py
+++ main.py
@@ -7,50 +7,88 @@ from config import config
 baseUrl = "https://store.steampowered.com/search/?specials=1&page="
 
 # Connect to MongoDB
-db = connect('steam-scrape', host=f'mongodb://{config["username"]}:{config["password"]}@ds145039.mlab.com', port=45039)
+db = connect('steam-scrape', host=f'mongodb://ds145039.mlab.com', port=45039, username=config['username'], password=config['password'])
 
-# Create connection, read raw html, close connection
-uClient = uReq(baseUrl + "1")
-page_html = uClient.read()
-uClient.close()
 
-# Parse the html
-page_soup = soup(page_html, "html.parser")
+# ===========================
+# Schema Class Definitions
+# ===========================
+class Games(Document):
+    gameName = StringField(required=True)
+    platform = ListField(StringField())
+    originalPrice = IntField(required=True)
+    discountedPrice = ListField(DictField(), required=True)
 
-# Grabs list of products
-search_results = page_soup.findAll("a", {"class": "search_result_row"})
 
-for i, product in enumerate(search_results):
-    try:
-        name = product.find("span", "title").text.strip()
-        original_price = int(str(product.find("strike")).replace("<strike>", "").replace("</strike>", "").replace("$", "").replace(".", ""))
-        sale_price = int(str(product.find("div", "search_price").contents[3]).replace("$", "").replace(".", "").strip())
+# ===========================
+# Main Function
+# ===========================
+def main():
+    # Create connection, read raw html, close connection
+    uClient = uReq(baseUrl + "1")
+    page_html = uClient.read()
+    uClient.close()
 
-        # Parse platforms
-        platforms = []
-        platforms_raw = product.findAll("span", "platform_img")
-        for span in platforms_raw:
-            if "win" in str(span):
-                platforms.append("win")
-            if "mac" in str(span):
-                platforms.append("mac")
-            if "linux" in str(span):
-                platforms.append("linux")
+    # Parse the html
+    page_soup = soup(page_html, "html.parser")
 
-        game_data = {
-                "gameName": name,
-                "platform": platforms,
-                "originalPrice": original_price,
-                "discountedPrice": [{
-                    "date": datetime.now(),
-                    "price": sale_price
-                }],
-            }
+    # Grabs list of products
+    search_results = page_soup.findAll("a", {"class": "search_result_row"})
 
-        print(game_data)
-    except Exception as e:
-        print("**********Problem with iteration " + str(i) + "**********")
-        print(e)
+    for i, product in enumerate(search_results):
+        try:
+            name = product.find("span", "title").text.strip()
+            original_price = int(str(product.find("strike")).replace("<strike>", "").replace("</strike>", "").replace("$", "").replace(".", ""))
+            sale_price = int(str(product.find("div", "search_price").contents[3]).replace("$", "").replace(".", "").strip())
 
-# Close DB connection
-db.close()
+            # Parse platforms
+            platforms = []
+            platforms_raw = product.findAll("span", "platform_img")
+            for span in platforms_raw:
+                if "win" in str(span):
+                    platforms.append("win")
+                if "mac" in str(span):
+                    platforms.append("mac")
+                if "linux" in str(span):
+                    platforms.append("linux")
+
+            game_data = {
+                    "gameName": name,
+                    "platform": platforms,
+                    "originalPrice": original_price,
+                    "discountedPrice": [{
+                        "date": datetime.now(),
+                        "price": sale_price
+                    }],
+                }
+
+            if Games.objects(gameName=game_data['gameName']):
+                # Push new price data
+                updatedObject = (
+                    Games.objects(gameName=game_data['gameName'])
+                    .modify(push__discountedPrice=game_data["discountedPrice"][0])
+                )
+                print(updatedObject)
+                print("Updated")
+            else:
+                # Create Games document and write to DB
+                game = Games(gameName=game_data['gameName'])
+                game.platform = game_data['platform']
+                game.originalPrice = game_data['originalPrice']
+                game.discountedPrice = game_data['discountedPrice']
+                game.save()
+                print("New game added to db")
+
+        except Exception as e:
+            print("**********Problem with iteration " + str(i) + "**********")
+            print(e)
+    # End search results for loop
+
+
+    # Close DB connection
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
+

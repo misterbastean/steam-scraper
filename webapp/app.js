4e6b6c5c11b78640adf49648be3ba0176ce0ada1
misterbastean <misterbastean@gmail.com>
2019-03-13 09:56:45 -0400

    Finish app.js boilerplate; add express-session

--- webapp/app.js
+++ webapp/app.js
@@ -11,7 +11,25 @@ const express         = require('express'),
 const indexRoutes = require('./routes/index'),
       authRoutes = require('./routes/auth')
 
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${config.database.username}:${config.database.password}@${config.database.dbhost}`, {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 
+// Passport config
+app.use(require('express-session')({secret: config.passport.secret, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// Routes
+app.use(indexRoutes);
+app.use(authRoutes);
 
 // Port/IP Listening
 const port = process.env.PORT || 3000;
